2e95b9838d082fb8c5d60671e8f208a19cfb9571
Dekatron322 <muritalaibrahim097@gmail.com>
2024-10-30 13:25:07 -0300

    add address

--- app/(auth)/(preoder)/address/add-address/page.tsx
+++ app/(auth)/(preoder)/address/add-address/page.tsx
@@ -34,162 +34,180 @@ export default function Web() {
 
       <section
         id="other-products"
-        className="paddings  w-full bg-[#080808] max-sm:px-3 max-sm:py-20 lg:h-screen lg:py-32"
+        className="paddings  w-full bg-[#080808] max-sm:px-3 max-sm:py-20 lg:h-auto lg:py-32"
       >
         <div className="flex  w-full  items-center justify-between md:px-10">
-          <div className="flex w-full flex-col  items-center justify-center rounded-3xl bg-[#151515] max-sm:rounded-lg max-sm:p-2  md:p-10">
-            <motion.button
-              whileHover={{ scale: 1.1 }}
-              whileTap={{ scale: 0.9 }}
-              className="font-regular mb-3  gap-2 rounded-lg border border-[#FFFFFF99] bg-[#FFFFFF80] px-4 py-2 text-xs uppercase text-[#FFFFFF] max-sm:py-2 "
-            >
-              Add New Address
-            </motion.button>
+          <div className="flex w-full flex-col   justify-center rounded-3xl bg-[#151515] max-sm:rounded-lg max-sm:p-2  md:p-10">
+            <p className="text-center text-xl text-[#FFFFFF]">Add New Address</p>
+            <p className="my-5 text-[#FFFFFF99]">Shipping Address</p>
 
-            <div className="flex h-full  rounded-lg  bg-[#FFFFFF1A]   max-sm:grid max-sm:gap-5  md:gap-10">
-              <div className="">
-                <ul className=" list-inside p-5">
-                  <li className="pb-2 text-sm text-[#FFFFFF99] max-sm:text-xs">
-                    <span className="text-[#FFFFFF80]">Name:</span> Sherif Adamu
-                  </li>
-                  <li className="pb-2 text-sm text-[#FFFFFF99] max-sm:text-xs">
-                    <span className="text-[#FFFFFF80]">Email:</span> Shereefadamu001@gmail.com
-                  </li>
-                  <li className="pb-2 text-sm text-[#FFFFFF99] max-sm:text-xs">
-                    <span className="text-[#FFFFFF80]">Address:</span> No5 ajika street, Sabo road, Unguwan Rimi,
-                    Kaduna, Nigeria
-                  </li>
-                </ul>
-                <div className="flex flex-col gap-2 px-5 pb-3">
-                  <div className="flex items-center gap-2" onClick={toggleShipping}>
-                    <motion.img
-                      src={isDefaultShipping ? "/CheckSquare.png" : "/CheckSquareEmpty.png"}
-                      width={18}
-                      height={18}
-                      alt=""
-                      initial={{ scale: 1.2, opacity: 0 }}
-                      animate={{ scale: 1, opacity: 1 }}
-                      transition={{ duration: 1, ease: "easeIn" }}
+            <div className="flex h-full w-full flex-col  rounded-lg    ">
+              <div className="grid w-full grid-cols-2 justify-between gap-5 max-md:grid-cols-1">
+                <div className="search-bg mb-2 h-[54.37px] w-full items-center justify-between rounded-lg border border-[#FFFFFF1A] px-3 py-4 hover:border-[#1B5EED4D] focus:border-[#1B5EED4D] focus:bg-[#FBFAFC] max-sm:mb-2">
+                  <div className="flex">
+                    <input
+                      type="text"
+                      id="vcn"
+                      placeholder="First Name (Required)"
+                      className="h-[24px] w-full bg-transparent text-base outline-none focus:outline-none"
+                      style={{ width: "100%", height: "24px" }}
                     />
-                    <p className="text-sm text-[#FFFFFF99] max-sm:text-xs">Set as default Shipping Address</p>
-                  </div>
-                  <div className="flex items-center gap-2" onClick={toggleBilling}>
-                    <motion.img
-                      src={isDefaultBilling ? "/CheckSquare.png" : "/CheckSquareEmpty.png"}
-                      width={18}
-                      height={18}
-                      alt=""
-                      initial={{ scale: 1.2, opacity: 0 }}
-                      animate={{ scale: 1, opacity: 1 }}
-                      transition={{ duration: 1, ease: "easeIn" }}
-                    />
-                    <p className="text-sm text-[#FFFFFF99] max-sm:text-xs">Set as default Billing Address</p>
                   </div>
                 </div>
-
-                <div className="flex border-b border-[#FFFFFF1A]"></div>
-                <div className="flex flex-col gap-2 px-5 py-3">
-                  <div className="flex items-center justify-between">
-                    <div className="flex items-center gap-2">
-                      <p className="text-sm text-[#FFFFFF80]">Edit</p>
-                      <motion.img
-                        src="/PencilSimple.png"
-                        width={18}
-                        height={18}
-                        alt=""
-                        initial={{ scale: 1.2, opacity: 0 }}
-                        animate={{ scale: 1, opacity: 1 }}
-                        transition={{ duration: 1, ease: "easeIn" }}
-                      />
-                    </div>
-                    <div className="flex items-center gap-2">
-                      <p className="text-sm text-[#FFFFFF80]">Delete</p>
-                      <motion.img
-                        src="/TrashNew.png"
-                        width={18}
-                        height={18}
-                        alt=""
-                        initial={{ scale: 1.2, opacity: 0 }}
-                        animate={{ scale: 1, opacity: 1 }}
-                        transition={{ duration: 1, ease: "easeIn" }}
-                      />
-                    </div>
+                <div className="search-bg mb-2 h-[54.37px] w-full items-center justify-between rounded-lg border border-[#FFFFFF1A] px-3 py-4 hover:border-[#1B5EED4D] focus:border-[#1B5EED4D] focus:bg-[#FBFAFC] max-sm:mb-2">
+                  <div className="flex">
+                    <input
+                      type="text"
+                      id="vcn"
+                      placeholder="Last Name (Required)"
+                      className="h-[24px] w-full bg-transparent text-base outline-none focus:outline-none"
+                      style={{ width: "100%", height: "24px" }}
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="search-bg mb-2 h-[54.37px] w-full items-center justify-between rounded-lg border border-[#FFFFFF1A] px-3 py-4 hover:border-[#1B5EED4D] focus:border-[#1B5EED4D] focus:bg-[#FBFAFC] max-sm:mb-2">
+                <div className="flex">
+                  <input
+                    type="text"
+                    id="vcn"
+                    placeholder="Email Address (Required)"
+                    className="h-[24px] w-full bg-transparent text-base outline-none focus:outline-none"
+                    style={{ width: "100%", height: "24px" }}
+                  />
+                </div>
+              </div>
+              <div className="search-bg mb-2 h-[54.37px] w-full items-center justify-between rounded-lg border border-[#FFFFFF1A] px-3 py-4 hover:border-[#1B5EED4D] focus:border-[#1B5EED4D] focus:bg-[#FBFAFC] max-sm:mb-2">
+                <div className="flex">
+                  <input
+                    type="text"
+                    id="vcn"
+                    placeholder="Street Address"
+                    className="h-[24px] w-full bg-transparent text-base outline-none focus:outline-none"
+                    style={{ width: "100%", height: "24px" }}
+                  />
+                </div>
+              </div>
+              <div className="grid w-full grid-cols-2 justify-between gap-5 max-md:grid-cols-1">
+                <div className="search-bg mb-2 h-[54.37px] w-full items-center justify-between rounded-lg border border-[#FFFFFF1A] px-3 py-4 hover:border-[#1B5EED4D] focus:border-[#1B5EED4D] focus:bg-[#FBFAFC] max-sm:mb-2">
+                  <div className="flex">
+                    <input
+                      type="text"
+                      id="vcn"
+                      placeholder="Zip Postal Code"
+                      className="h-[24px] w-full bg-transparent text-base outline-none focus:outline-none"
+                      style={{ width: "100%", height: "24px" }}
+                    />
+                  </div>
+                </div>
+                <div className="search-bg mb-2 h-[54.37px] w-full items-center justify-between rounded-lg border border-[#FFFFFF1A] px-3 py-4 hover:border-[#1B5EED4D] focus:border-[#1B5EED4D] focus:bg-[#FBFAFC] max-sm:mb-2">
+                  <div className="flex">
+                    <input
+                      type="text"
+                      id="vcn"
+                      placeholder="phone (Required)"
+                      className="h-[24px] w-full bg-transparent text-base outline-none focus:outline-none"
+                      style={{ width: "100%", height: "24px" }}
+                    />
                   </div>
                 </div>
               </div>
             </div>
+            <div className="my-3 flex w-full items-center justify-center gap-2 self-center" onClick={toggleBillingTwo}>
+              <motion.img
+                src={isDefaultBillingTwo ? "/CheckSquareEmpty.png" : "/CheckSquare.png"}
+                width={18}
+                height={18}
+                alt=""
+                initial={{ scale: 1.2, opacity: 0 }}
+                animate={{ scale: 1, opacity: 1 }}
+                transition={{ duration: 1, ease: "easeIn" }}
+              />
+              <p className="text-sm text-[#FFFFFF99] max-sm:text-xs">Set as default Billing Address</p>
+            </div>
 
-            <div className="mt-3 flex h-full rounded-lg  bg-[#FFFFFF1A]   max-sm:grid max-sm:gap-5  md:gap-10">
-              <div className="">
-                <ul className=" list-inside p-5">
-                  <li className="pb-2 text-sm text-[#FFFFFF99] max-sm:text-xs">
-                    <span className="text-[#FFFFFF80]">Name:</span> Sherif Adamu
-                  </li>
-                  <li className="pb-2 text-sm text-[#FFFFFF99] max-sm:text-xs">
-                    <span className="text-[#FFFFFF80]">Email:</span> Shereefadamu001@gmail.com
-                  </li>
-                  <li className="pb-2 text-sm text-[#FFFFFF99] max-sm:text-xs">
-                    <span className="text-[#FFFFFF80]">Address:</span> No5 ajika street, Sabo road, Unguwan Rimi,
-                    Kaduna, Nigeria
-                  </li>
-                </ul>
-                <div className="flex flex-col gap-2 px-5 pb-3">
-                  <div className="flex items-center gap-2" onClick={toggleShippingTwo}>
-                    <motion.img
-                      src={isDefaultShippingTwo ? "/CheckSquareEmpty.png" : "/CheckSquare.png"}
-                      width={18}
-                      height={18}
-                      alt=""
-                      initial={{ scale: 1.2, opacity: 0 }}
-                      animate={{ scale: 1, opacity: 1 }}
-                      transition={{ duration: 1, ease: "easeIn" }}
+            <p className="my-5 text-[#FFFFFF99]">Billing Address</p>
+
+            <div className="flex h-full w-full flex-col  rounded-lg    ">
+              <div className="grid w-full grid-cols-2 justify-between gap-5 max-md:grid-cols-1">
+                <div className="search-bg mb-2 h-[54.37px] w-full items-center justify-between rounded-lg border border-[#FFFFFF1A] px-3 py-4 hover:border-[#1B5EED4D] focus:border-[#1B5EED4D] focus:bg-[#FBFAFC] max-sm:mb-2">
+                  <div className="flex">
+                    <input
+                      type="text"
+                      id="vcn"
+                      placeholder="First Name (Required)"
+                      className="h-[24px] w-full bg-transparent text-base outline-none focus:outline-none"
+                      style={{ width: "100%", height: "24px" }}
                     />
-                    <p className="text-sm text-[#FFFFFF99] max-sm:text-xs">Set as default Shipping Address</p>
-                  </div>
-                  <div className="flex items-center gap-2" onClick={toggleBillingTwo}>
-                    <motion.img
-                      src={isDefaultBillingTwo ? "/CheckSquareEmpty.png" : "/CheckSquare.png"}
-                      width={18}
-                      height={18}
-                      alt=""
-                      initial={{ scale: 1.2, opacity: 0 }}
-                      animate={{ scale: 1, opacity: 1 }}
-                      transition={{ duration: 1, ease: "easeIn" }}
-                    />
-                    <p className="text-sm text-[#FFFFFF99] max-sm:text-xs">Set as default Billing Address</p>
                   </div>
                 </div>
-
-                <div className="flex border-b border-[#FFFFFF1A]"></div>
-                <div className="flex flex-col gap-2 px-5 py-3">
-                  <div className="flex items-center justify-between">
-                    <div className="flex items-center gap-2">
-                      <p className="text-sm text-[#FFFFFF80]">Edit</p>
-                      <motion.img
-                        src="/PencilSimple.png"
-                        width={18}
-                        height={18}
-                        alt=""
-                        initial={{ scale: 1.2, opacity: 0 }}
-                        animate={{ scale: 1, opacity: 1 }}
-                        transition={{ duration: 1, ease: "easeIn" }}
-                      />
-                    </div>
-                    <div className="flex items-center gap-2">
-                      <p className="text-sm text-[#FFFFFF80]">Delete</p>
-                      <motion.img
-                        src="/TrashNew.png"
-                        width={18}
-                        height={18}
-                        alt=""
-                        initial={{ scale: 1.2, opacity: 0 }}
-                        animate={{ scale: 1, opacity: 1 }}
-                        transition={{ duration: 1, ease: "easeIn" }}
-                      />
-                    </div>
+                <div className="search-bg mb-2 h-[54.37px] w-full items-center justify-between rounded-lg border border-[#FFFFFF1A] px-3 py-4 hover:border-[#1B5EED4D] focus:border-[#1B5EED4D] focus:bg-[#FBFAFC] max-sm:mb-2">
+                  <div className="flex">
+                    <input
+                      type="text"
+                      id="vcn"
+                      placeholder="Last Name (Required)"
+                      className="h-[24px] w-full bg-transparent text-base outline-none focus:outline-none"
+                      style={{ width: "100%", height: "24px" }}
+                    />
                   </div>
                 </div>
               </div>
+              <div className="search-bg mb-2 h-[54.37px] w-full items-center justify-between rounded-lg border border-[#FFFFFF1A] px-3 py-4 hover:border-[#1B5EED4D] focus:border-[#1B5EED4D] focus:bg-[#FBFAFC] max-sm:mb-2">
+                <div className="flex">
+                  <input
+                    type="text"
+                    id="vcn"
+                    placeholder="Email Address (Required)"
+                    className="h-[24px] w-full bg-transparent text-base outline-none focus:outline-none"
+                    style={{ width: "100%", height: "24px" }}
+                  />
+                </div>
+              </div>
+              <div className="search-bg mb-2 h-[54.37px] w-full items-center justify-between rounded-lg border border-[#FFFFFF1A] px-3 py-4 hover:border-[#1B5EED4D] focus:border-[#1B5EED4D] focus:bg-[#FBFAFC] max-sm:mb-2">
+                <div className="flex">
+                  <input
+                    type="text"
+                    id="vcn"
+                    placeholder="Street Address"
+                    className="h-[24px] w-full bg-transparent text-base outline-none focus:outline-none"
+                    style={{ width: "100%", height: "24px" }}
+                  />
+                </div>
+              </div>
+              <div className="grid w-full grid-cols-2 justify-between gap-5 max-md:grid-cols-1">
+                <div className="search-bg mb-2 h-[54.37px] w-full items-center justify-between rounded-lg border border-[#FFFFFF1A] px-3 py-4 hover:border-[#1B5EED4D] focus:border-[#1B5EED4D] focus:bg-[#FBFAFC] max-sm:mb-2">
+                  <div className="flex">
+                    <input
+                      type="text"
+                      id="vcn"
+                      placeholder="Zip Postal Code"
+                      className="h-[24px] w-full bg-transparent text-base outline-none focus:outline-none"
+                      style={{ width: "100%", height: "24px" }}
+                    />
+                  </div>
+                </div>
+                <div className="search-bg mb-2 h-[54.37px] w-full items-center justify-between rounded-lg border border-[#FFFFFF1A] px-3 py-4 hover:border-[#1B5EED4D] focus:border-[#1B5EED4D] focus:bg-[#FBFAFC] max-sm:mb-2">
+                  <div className="flex">
+                    <input
+                      type="text"
+                      id="vcn"
+                      placeholder="phone (Required)"
+                      className="h-[24px] w-full bg-transparent text-base outline-none focus:outline-none"
+                      style={{ width: "100%", height: "24px" }}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="flex w-full flex-col  items-center justify-center rounded-3xl bg-[#151515] max-sm:rounded-lg max-sm:p-2  md:pt-10">
+              <motion.button
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.9 }}
+                className="font-regular mb-3  gap-2 rounded-lg border border-[#FFFFFF99] bg-[#FFFFFF80] px-20 py-2 text-xs uppercase text-[#FFFFFF] max-sm:py-2 "
+              >
+                Save
+              </motion.button>
             </div>
           </div>
         </div>
