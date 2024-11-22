176e55afb862461df3117d0ec316971c26f8a033
Dekatron322 <muritalaibrahim097@gmail.com>
2024-11-22 11:08:49 +0100

    contact us

---
+++ app/(auth)/contact-us/page.tsx
@@ -0,0 +1,72 @@
+"use client"
+import Footer from "components/Footer/Footer"
+import AOS from "aos"
+import "aos/dist/aos.css"
+import Image from "next/image"
+import Navbar from "components/Navbar/Navbar"
+import { useEffect } from "react"
+import Accordion from "components/Accordion/Accordion"
+import { motion } from "framer-motion"
+import MainFooter from "components/Footer/MainFooter"
+
+export default function Web() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000, // Animation duration
+      once: true, // Only animate elements once
+    })
+  }, [])
+
+  return (
+    <section className="bg-black">
+      <Navbar />
+
+      <section className="paddings  w-full bg-[#080808] max-sm:px-3 " id="contact">
+        <div className="  w-full     py-20">
+          <div className="flex flex-col  items-center justify-center">
+            <p className=" text-center text-[#FFFFFF99]">Contact us</p>
+            <p className="my-6 flex text-center  text-[32px]  font-bold  text-[#FFFFFF] max-md:text-2xl">
+              Have Questions?
+            </p>
+            <div className="input-field ">
+              <input
+                type="text"
+                id="placement"
+                placeholder="Full Name"
+                className="bg-transparent  text-xs text-white outline-none focus:outline-none"
+                style={{ width: "100%" }}
+              />
+            </div>
+
+            <div className="input-field my-7">
+              <input
+                type="text"
+                id="placement"
+                placeholder="Email"
+                className="bg-transparent text-xs  text-white outline-none focus:outline-none"
+                style={{ width: "100%" }}
+              />
+            </div>
+
+            <div className="text-area ">
+              <textarea
+                id="username"
+                placeholder="Case Title"
+                className="min-h-[200px] bg-transparent text-xs text-white outline-none focus:outline-none"
+                style={{ width: "100%", height: "24px" }}
+              ></textarea>
+            </div>
+
+            <button className="font-regular mt-7 flex w-[60%] items-center justify-center gap-2  rounded-lg border border-[#FF3B30] bg-[#FF3B30] px-4 py-4 uppercase text-[#FFFFFF] max-sm:w-full ">
+              Submit
+            </button>
+          </div>
+        </div>
+      </section>
+
+      <MainFooter />
+
+      <Footer />
+    </section>
+  )
+}
