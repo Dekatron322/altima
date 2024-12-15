188f5b3361ce5290ea2a2629bf51175d89ab9c5b
Dekatron322 <muritalaibrahim097@gmail.com>
2024-12-15 14:08:36 +0100

    terms and conditions

--- app/(auth)/terms-and-conditions/page.tsx
+++ app/(auth)/terms-and-conditions/page.tsx
@@ -11,49 +11,171 @@ export default function Web() {
 
   const faqData = [
     {
-      title: "1. Information We Collect",
-      content:
-        "We may collect the following types of information ▪️Personal Information: Name, email address, phone number, shipping address, and payment details necessary for processing your preorder. ▪️Device Information: IP address, browser type, operating system, and other technical data to enhance user experience and improve website performance.▪️Usage Data: Information on how you use our website, such as page views and navigation patterns.",
+      title: "1. General Terms",
+      content: (
+        <div>
+          
+          <ul className="grid gap-3">
+            <li><strong>▪️ Acceptance of Terms:</strong> By accessing this website or placing a preorder, you agree to these Terms and Conditions. If you do not agree, please refrain from using our services.
+            </li>
+            <li className="px-2"><strong>▪️ Modifications:</strong>  Smart Haven Systems Private Limited reserves the right to modify these Terms at any time. We will post updates on this page, and your continued use of the site signifies your acceptance of the modified terms.
+            </li>   
+          </ul>
+        </div>
+      ),
     },
     {
-      title: "2. How We Use Your Information",
-      content:
-        "To cancel your pre-order, please reach out to our support team at [ *support email* ] or call us at [ *support phone number* ]. Alternatively, you can log in to your account and follow the instructions under the My Orders section to cancel.",
+      title: "2. Preorder Terms",
+      content: (
+        <div>
+          <ul className="grid gap-3">
+            <li><strong>▪️ Preorder Process:</strong> By placing a preorder, you are reserving an Altima product in advance. Preordering requires a deposit payment, which secures your place in our production queue.
+            </li>
+            <li className="px-2"><strong>▪️ Availability:</strong> Preordering does not guarantee immediate availability. Estimated production and delivery timelines are provided, but actual delivery dates may vary based on production schedules and supply chain factors.
+            </li> 
+            <li className="px-2"><strong>▪️ Non-Binding Agreement:</strong> Placing a preorder does not constitute a binding purchase contract. Smart Haven Systems Private Limited reserves the right to cancel or refund preorders in specific cases, including but not limited to production delays, technical issues, or unforeseen circumstances.
+            </li>   
+          </ul>
+        </div>
+      ),
     },
     {
-      title: "3. Sharing Your Information",
-      content:
-        "The pre-order process is simple. Select your preferred Altima model (Core or Elite), customize your door with the options you need, review the details, and place a 30% deposit to secure your order. We’ll keep you informed as your order progresses toward delivery.",
+      title: "3. Payment Terms",
+      content: (
+        <div>
+          <ul className="grid gap-3">
+            <li><strong>▪️ Deposit:</strong>  Preorders require a 30% non-refundable deposit at the time of booking. This deposit is applied toward the total product price.
+            </li>
+            <li className="px-2"><strong>▪️ Final Payment:</strong>  The remaining balance is due prior to delivery. A final payment notice will be sent to the email address provided at the time of preorder, with a specified payment deadline.
+            </li> 
+            <li className="px-2"><strong>▪️ Accepted Payment Methods:</strong> We accept various payment methods, including credit cards, debit cards, and bank transfers. Specific payment instructions will be provided during the preorder process.
+            </li>
+            <li className="px-2"><strong>▪️ Non-Refundable Deposit:</strong> Please note that the preorder deposit is non-refundable. However, in cases where Smart Haven Systems Private Limited initiates a cancellation, the deposit will be refunded in full.
+            </li>   
+          </ul>
+        </div>
+      ),
     },
     {
-      title: "4. Data Security",
-      content:
-        "If there are any production delays, we will notify you promptly and provide an updated timeline. You can choose to wait for your order or request a full refund of your deposit if the delay is inconvenient.",
+      title: "4. Pricing and Tax",
+      content: (
+        <div>
+          <ul className="grid gap-3">
+            <li><strong>▪️ Pricing:</strong> All prices displayed on our website are exclusive of applicable taxes. Taxes and additional fees will be calculated and added at checkout based on your location.
+            </li>
+            <li className="px-2"><strong>▪️ Price Adjustments:</strong>   Prices are subject to change at the discretion of Smart Haven Systems Private Limited. However, preorder prices at the time of booking will be honored for confirmed orders.
+            </li>  
+          </ul>
+        </div>
+      ),
     },
     {
-      title: "5. Cookies and Tracking Technologies",
-      content:
-        "Our smart doors come with a 1-year standard warranty covering any manufacturing defects. We also offer an optional extended warranty for an additional 2 years, providing a total of 3 years of coverage for extra peace of mind.",
+      title: "5. Delivery and Shipping",
+      content: (
+        <div>
+          <ul className="grid gap-3">
+            <li><strong>▪️ Estimated Delivery:</strong> Altima products are estimated to ship within 4-5 months after the preorder window closes. We will make every effort to meet this timeline; however, delivery dates may vary due to production schedules and external factors.
+            </li>
+            <li className="px-2"><strong>▪️ Shipping Notification:</strong> Once your order is ready for shipment, you will receive a notification email with tracking information and estimated arrival dates.
+            </li>
+            <li className="px-2"><strong>▪️ Shipping Costs:</strong> Shipping charges will be added to your final payment and will be disclosed in advance.
+            </li> 
+            <li className="px-2"><strong>▪️ Risk of Loss:</strong> Risk of loss or damage passes to the customer upon shipment. Smart Haven Systems Private Limited is not responsible for items lost or damaged in transit, but we will assist you with any claims to the shipping provider.
+            </li>   
+          </ul>
+        </div>
+      ),
     },
     {
-      title: "6. Your Rights",
-      content:
-        "We provide professional installation support in select areas to ensure your Altima smart door is installed seamlessly. Contact our support team to *discuss installation options in your location and any associated costs.",
+      title: "6. Cancellations and Refunds",
+      content: (
+        <div>
+          <ul className="grid gap-3">
+            <li><strong>▪️ Customer-Initiated Cancellations:</strong> Preorder deposits are non-refundable, and customer-initiated cancellations will forfeit the deposit.
+            </li>
+            <li className="px-2"><strong>▪️ Company-Initiated Cancellations:</strong> In cases where Smart Haven Systems Private Limited initiates the cancellation of a preorder due to unforeseen circumstances, we will issue a full refund of the deposit.
+            </li>
+            <li className="px-2"><strong>▪️ Refunds:</strong> All eligible refunds will be processed within 7-10 business days and will be credited back to the original payment method.
+            </li>  
+          </ul>
+        </div>
+      ),
     },
     {
-      title: "7. Third-Party Links",
-      content:
-        "Yes, we offer flexible financing and payment plans to make your purchase more manageable. Reach out to our finance department at [ *finance contact email/phone* ] for more details and to find a plan that suits your needs.",
+      title: "7. Product Warranty and Returns",
+      content: (
+        <div>
+          <ul className="grid gap-3">
+            <li><strong>▪️ Limited Warranty:</strong> Altima products come with a limited warranty covering manufacturing defects for a specified period from the date of delivery. Warranty details are provided with your product documentation.
+
+            </li>
+            <li className="px-2"><strong>▪️ Exclusions:</strong>  The warranty does not cover damage caused by misuse, modifications, or accidents.
+
+            </li>
+            <li className="px-2"><strong>▪️ Refunds:</strong> ▪️ Returns: Returns are not accepted on preorder products except in cases covered under the warranty for manufacturing defects.
+
+            </li>  
+          </ul>
+        </div>
+      ),
     },
     {
-      title: "8. Policy Updates",
-      content:
-        "Yes, we offer flexible financing and payment plans to make your purchase more manageable. Reach out to our finance department at [ *finance contact email/phone* ] for more details and to find a plan that suits your needs.",
+      title: "8. Limitation of Liability",
+      content: (
+        <div>
+          <ul className="grid gap-3">
+            <li><strong>▪️ Disclaimer of Warranties:</strong> While we strive to provide accurate information, Smart Haven Systems Private Limited does not guarantee that the website or its content is free of errors. We do not warrant that our products or services will meet your specific expectations.
+            </li>
+            <li className="px-2"><strong>▪️ Limitation of Liability:</strong> To the fullest extent permitted by law, Smart Haven Systems Private Limited and its affiliates, officers, employees, and agents shall not be liable for any indirect, incidental, special, or consequential damages resulting from the use of our website, products, or services.
+            </li>
+          </ul>
+        </div>
+      ),
     },
     {
-      title: "9. Contact Us",
-      content:
-        "Yes, we offer flexible financing and payment plans to make your purchase more manageable. Reach out to our finance department at [ *finance contact email/phone* ] for more details and to find a plan that suits your needs.",
+      title: "9. Intellectual Property",
+      content: (
+        <div>
+          <ul className="grid gap-3">
+            <li><strong>▪️ Ownership:</strong>  All website content, including text, images, graphics, logos, and software, is the property of Smart Haven Systems Private Limited and is protected by applicable copyright and intellectual property laws.
+            </li>
+            <li className="px-2"><strong>▪️ Restrictions:</strong>  You are prohibited from reproducing, distributing, or using any content on this website without explicit written permission from Smart Haven Systems Private Limited.
+            </li>
+          </ul>
+        </div>
+      ),
+    },
+    {
+      title: "10. Governing Law and Dispute Resolution",
+      content: (
+        <div>
+          <ul className="grid gap-3">
+            <li><strong>▪️ Governing Law:</strong> These Terms and Conditions are governed by and construed in accordance with the laws of India.
+
+            </li>
+            <li className="px-2"><strong>▪️ Dispute Resolution:</strong> Any disputes arising out of or related to these Terms shall be resolved through arbitration in Mumbai, India, in accordance with the Arbitration and Conciliation Act, 1996.
+
+            </li>
+          </ul>
+        </div>
+      ),
+    },
+    {
+      title: "11. Contact Information",
+      content: (
+        <div>
+          
+          <ul className="">
+            
+            <p className="mb-3">Our team is here to assist you with any installation-related questions or issues. For assistance, please contact:
+            </p>
+            <li><strong>Email:</strong> warranty@smarthavensystems.com</li>
+            <li className="my-3"><strong>Phone:</strong> 022-6971-8365</li>
+            <li><strong>Address:</strong> Seawoods, Navi Mumbai 400706, India</li>
+          </ul>
+           
+        </div>
+      ),
     },
   ]
 
