ab8cf4b7f3add6db7f72395b36129899613df676
Dekatron322 <muritalaibrahim097@gmail.com>
2024-12-22 11:40:22 +0100

    build

--- components/ImageModal/ImageModal.tsx
+++ components/ImageModal/ImageModal.tsx
@@ -1,5 +1,5 @@
 import { motion } from 'framer-motion';
-import { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { GoChevronLeft, GoChevronRight } from "react-icons/go";
 
 
